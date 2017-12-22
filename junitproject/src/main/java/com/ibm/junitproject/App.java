d0c5e00e6ce9b2c0935592ddc42dea7549395838
3370_30310 <3370_30310@noemail.com>
2017-12-22 15:04:54 +0530

    Initial Commit

---
+++ junitproject/src/main/java/com/ibm/junitproject/App.java
@@ -0,0 +1,16 @@
+package com.ibm.junitproject;
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    
+        System.out.println( "Hello World!" );
+    }
+}
